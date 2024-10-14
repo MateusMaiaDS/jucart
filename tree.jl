b42dfc2fb4314da413c842849e1bb2a8976e71a5
MateusMaiaDS <mmateusmaiads@gmail.com>
2024-10-14 23:59:43 +0100

    adding code for the getting a parent of a terminal node, as the code does not store memory about the parent the search start from the root@

--- tree.jl
+++ tree.jl
@@ -10,8 +10,12 @@ end
 
 mutable struct Leaf <: Node
     μ::Float64
-    Σr::Float64
-    ∑r²::Float64
+    #Σr::Float64 --- This might not be necessary the Leaf should store only the \mu value.
+    #∑r²::Float64 --- Same
+end
+
+mutable struct Tree
+    root::Node
 end
 
 function get_leaf_nodes(node::Node)
@@ -22,3 +26,27 @@ function get_leaf_nodes(node::Node)
         reduce(vcat,[get_leaf_nodes(node.left),get_leaf_nodes(node.right)])
      end
 end
+
+# This function initialise the search of the node by the root for the tree
+#(remember that the leaves do not store any information about the parents so, we save memory in this way)
+function get_my_parent(node::Node, tree::Tree)
+    get_my_parent(node,tree.root)
+end
+
+# It is important to notice that this will check if the evaluated parent (starting from the root)
+# as a candidate of the node that we want to evaluate;
+function get_my_parent(node::Node, parent_candidate::Branch)
+    if (node==parent_candidate.left) || (node==parent_candidate.right)
+        return parent_candidate
+    else 
+        isa(get_my_parent(node,parent_candidate.left),Nothing)?
+            get_my_parent(node,parent_candidate.right):get_my_parent(node,parent_candidate.left)
+    end
+end 
+
+# Simple, if the parent candidate is a Leaf, it cannot be a parent!
+function get_my_parent(node::Node, parent_candidate::Leaf)
+    return nothing
+end
+
+
