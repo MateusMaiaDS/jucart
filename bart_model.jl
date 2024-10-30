21088f7ab8d3d995e74725f8687bf5b43cec836e
Mateus Maia Marques <mateus.maiamarques@glasgow.ac.uk>
2024-10-30 18:03:59 +0000

    estimating hyperparameters

--- bart_model.jl
+++ bart_model.jl
@@ -1,16 +1,11 @@
-struct Hypers
-    α::Float64
-    β::Float64
-    τ_μ::Float64
-    a_τ::Float64
-    d_τ::Float64
-end
+using Distributions
+
 
 struct TrainData
     n::Int64
     p::Int64
     x_train::Matrix{Float64}
-    y_train::Matrix{Float64} 
+    y_train::AbstractMatrix
     xmin::Matrix{Float64}
     xmax::Matrix{Float64}
     xcut::Matrix{Float64}
@@ -33,6 +28,31 @@ struct MCMC
 end
 
 
+struct Hypers
+    m::Int64
+    α::Float64
+    β::Float64
+    k::Float64
+    τ_μ::Float64
+    q::Float64
+    a_τ::Float64
+    d_τ::Float64
+    function Hypers(td::TrainData; m = 200,α = 0.95, β = 2.0,k = 2.0, q = 0.9,a_τ = 3)
+        qchi = quantile(Chisq(a_τ),1-q)
+        lambda = (td.τ_OLS^(-1)*qchi)/a_τ
+        d_τ = lambda*a_τ/2
+        
+        if isa(td.y_train,Matrix{Int})
+            τ_μ = (m*k^2)/9
+        else 
+            τ_μ = (4*m*k^2)/(maximum(td.y_train)-minimum(td.y_train))
+        end
+        
+        new(m,α,β,k,τ_μ,q,a_τ,d_τ)
+    end              
+end
+
+
 struct BartModel
     hypers::Hypers
     td::TrainData
@@ -41,20 +61,23 @@ end
 
 
 
-function TrainData(x_train::Matrix{Float64},y_train::Matrix{Float64},numcut::Int64,usequant::Bool)
+function TrainData(x_train::Matrix{Float64},y_train::AbstractMatrix,numcut::Int64,usequant::Bool)
     n = length(y_train)
     p = size(x_train,2)
     xmin = minimum(x_train,dims = 1)
     xmax = maximum(x_train,dims = 1)
     ymin = minimum(y_train)
     ymax = maximum(y_train)
-    x_train = scale_X!(x_train,xmin,xmax)
-    y_train = normalize_y!(y_train,ymin,ymax)
+    scale_X!(x_train,xmin,xmax)
+    #if isa(y_train,Matrix{Float64})
+    #    normalize_y!(y_train,ymin,ymax)
+    #end
+
     τ_OLS = naive_tau(x_train,y_train)
     if usequant
         xcut = get_xcut(x_train,numcut)
     else 
-        xcut =get_xcut(x_train,xmin,xmax,numcut)
+        xcut = get_xcut(x_train,xmin,xmax,numcut)
     end
 
     TrainData(n,p,x_train,y_train,xmin,xmax,xcut,ymin,ymax,τ_OLS)
--- running_header.jl
+++ running_header.jl
@@ -13,4 +13,5 @@ y = 5 .+ 2 .*x[:,1] .+ randn(n_)              # Response vector#
 y = reshape(y, :, 1)        # Explicitly reshape y into a 4x1 matrix
 
 td_::TrainData = TrainData(x,y,100,false)
+hypers_::Hypers =  Hypers(td_)
 
