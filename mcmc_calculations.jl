d6a035d57abe8f7746f9673a64dfaddbb78f4c22
Mateus Maia Marques <mateus.maiamarques@glasgow.ac.uk>
2025-03-12 09:32:52 +0000

    generlasing the mll method

--- mcmc_calculations.jl
+++ mcmc_calculations.jl
@@ -2,13 +2,13 @@ function suffstats(tree_residuals::Vector{Float64},X_tree::Matrix{Float64},bs::S
 
     number_leaves = size(X_tree,2)
     omega = (bs.σ^(2)) ./ (sum(X_tree, dims = 1).*bm.hypers.σ_μ^2 .+ bs.σ^2) # A vector with σ²_μ/(σ²+nₗσ²_μ) for each terminal node
-    r_sum = transpose(X_tree)*tree_residuals
+    r_sum = transpose(X_tree)*tree_residuals # Doing this operation is the same adding up residuals within terminal nodes ∑ᵢrᵢ
 
     BartSufficientStats(number_leaves,omega,r_sum)
 end
 
 function mll(ss::BartSufficientStats,bs::StandardBartState,bm::BartModel,index::Int)
-    return -0.5*log(2*pi*bm.hypers.σ_μ^2)+0.5*log(bs.σ/ss.omega[index])+0.5*(ss.r_sum[index]^2)/(bs.σ*ss.omega[index])
+    return -0.5*log(2*pi*bm.hypers.σ_μ^2)+0.5*log(bs.σ/ss.omega[indexes])+0.5*(ss.r_sum[indexes]^2)/(bs.σ*ss.omega[indexes])
 end
 
 
